79c04cfc6393801b3d30e34f6d3bc4037ec9ecdc
Remco Haszing <remcohaszing@gmail.com>
2023-03-16 12:27:25 +0100

    Remove redundant types

--- src/types.ts
+++ src/types.ts
@@ -1,32 +1,6 @@
-import { Postcss } from 'postcss';
-import parse from 'postcss-selector-parser';
 import { Config } from 'tailwindcss';
 import { State } from 'tailwindcss-language-service';
-import expandApplyAtRules from 'tailwindcss/src/lib/expandApplyAtRules.js';
-import { generateRules } from 'tailwindcss/src/lib/generateRules.js';
-import { createContext, JitContext } from 'tailwindcss/src/lib/setupContextUtils.js';
 
 export interface JitState extends State {
   config: Config;
-  jitContext: JitContext;
-  modules: {
-    postcss: {
-      version: string;
-      module: Postcss;
-    };
-    postcssSelectorParser: {
-      module: typeof parse;
-    };
-    jit: {
-      generateRules: {
-        module: typeof generateRules;
-      };
-      createContext: {
-        module: typeof createContext;
-      };
-      expandApplyAtRules: {
-        module: typeof expandApplyAtRules;
-      };
-    };
-  };
 }
